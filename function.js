af9b64cd10b8b1bf17f7e01493b5ed60de8201f2
martinpstewart <155626500+martinpstewart@users.noreply.github.com>
2024-03-27 09:41:12 +0000

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-window.function = function (name,image) {
+window.function = function (name, image) {
     // FIDELITY MAPPING
     const fidelityMap = {
         low: 1,
@@ -8,281 +8,95 @@ window.function = function (name,image) {
 
     // DYNAMIC VALUES
     name = name.value ?? "No HTML set.";
-	image = image.value ?? "No Image";
+    image = image.value ?? "No Image";
 
-    // Populate HTML table with provided name
+    // HTML TABLE TEMPLATE
     const tableHTML = `
-       <!DOCTYPE html>
-	<head>
-		<meta charset="utf-8" />
-		<style>
-			.invoice-box {
-				max-width: 800px;
-				margin: auto;
-				padding: 30px;
-				border: 1px solid #eee;
-				box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
-				font-size: 16px;
-				line-height: 24px;
-				font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
-				color: #555;
-			}
+        <table>
+            <tr>
+                <td class="title">
+                    <img src="${image}" style="width: 100%; max-width: 300px"/>
+                </td>
+                <td>
+                    Invoice #: 123<br/>
+                    Created: January 1, 2023<br/>
+                    Due: February 1, 2023
+                </td>
+            </tr>
+        </table>
 
-			.invoice-box table {
-				width: 100%;
-				line-height: inherit;
-				text-align: left;
-			}
+        <tr class="information">
+            <td colspan="2">
+                <table>
+                    <tr>
+                        <td>
+                            ${name}<br/>
+                            12345 Sunny Road<br/>
+                            Sunnyville, CA 12345
+                        </td>
+                        <td>
+                            Acme Corp.<br/>
+                            John Doe<br/>
+                            john@example.com
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
 
-			.invoice-box table td {
-				padding: 5px;
-				vertical-align: top;
-			}
+        <!-- Other table rows -->
 
-			.invoice-box table tr td:nth-child(2) {
-				text-align: right;
-			}
+        <tr class="total">
+            <td></td>
+            <td>Total: $385.00</td>
+        </tr>
+    </table>`;
 
-			.invoice-box table tr.top table td {
-				padding-bottom: 20px;
-			}
-
-			.invoice-box table tr.top table td.title {
-				font-size: 45px;
-				line-height: 45px;
-				color: #333;
-			}
-
-			.invoice-box table tr.information table td {
-				padding-bottom: 40px;
-			}
-
-			.invoice-box table tr.heading td {
-				background: #eee;
-				border-bottom: 1px solid #ddd;
-				font-weight: bold;
-			}
-
-			.invoice-box table tr.details td {
-				padding-bottom: 20px;
-			}
-
-			.invoice-box table tr.item td {
-				border-bottom: 1px solid #eee;
-			}
-
-			.invoice-box table tr.item.last td {
-				border-bottom: none;
-			}
-
-			.invoice-box table tr.total td:nth-child(2) {
-				border-top: 2px solid #eee;
-				font-weight: bold;
-			}
-
-			@media only screen and (max-width: 600px) {
-				.invoice-box table tr.top table td {
-					width: 100%;
-					display: block;
-					text-align: center;
-				}
-
-				.invoice-box table tr.information table td {
-					width: 100%;
-					display: block;
-					text-align: center;
-				}
-			}
-
-			/** RTL **/
-			.invoice-box.rtl {
-				direction: rtl;
-				font-family: Tahoma, 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
-			}
-
-			.invoice-box.rtl table {
-				text-align: right;
-			}
-
-			.invoice-box.rtl table tr td:nth-child(2) {
-				text-align: left;
-			}
-		</style>
-	</head>
-
-	<body>
-		<div class="invoice-box">
-			<table cellpadding="0" cellspacing="0">
-				<tr class="top">
-					<td colspan="2">
-						<table>
-							<tr>
-								<td class="title">
-									<img
-										src="${image}"
-										style="width: 100%; max-width: 300px"
-									/>
-								</td>
-
-								<td>
-									Invoice #: 123<br />
-									Created: January 1, 2023<br />
-									Due: February 1, 2023
-								</td>
-							</tr>
-						</table>
-					</td>
-				</tr>
-
-				<tr class="information">
-					<td colspan="2">
-						<table>
-							<tr>
-								<td>
-									${name}<br />
-									12345 Sunny Road<br />
-									Sunnyville, CA 12345
-								</td>
-
-								<td>
-									Acme Corp.<br />
-									John Doe<br />
-									john@example.com
-								</td>
-							</tr>
-						</table>
-					</td>
-				</tr>
-
-				<tr class="heading">
-					<td>Payment Method</td>
-
-					<td>Check #</td>
-				</tr>
-
-				<tr class="details">
-					<td>Check</td>
-
-					<td>1000</td>
-				</tr>
-
-				<tr class="heading">
-					<td>Item</td>
-
-					<td>Price</td>
-				</tr>
-
-				<tr class="item">
-					<td>Website design</td>
-
-					<td>$300.00</td>
-				</tr>
-
-				<tr class="item">
-					<td>Hosting (3 months)</td>
-
-					<td>$75.00</td>
-				</tr>
-
-				<tr class="item last">
-					<td>Domain name (1 year)</td>
-
-					<td>$10.00</td>
-				</tr>
-
-				<tr class="total">
-					<td></td>
-
-					<td>Total: $385.00</td>
-				</tr>
-			</table>
-		</div>
-	</body>
-    `;
-
+    // CUSTOM CSS
     const customCSS = `
-    body {
-        margin: 0!important
-    }
+        body {
+            margin: 0!important
+        }
+        /* CSS Styles for the download button and scrollbar */
 
-    button#download {
-        position: fixed;
-        border-radius: 0.5rem;
-        font-size: 14px;
-        font-weight: 600;
-        line-height: 1.5rem;
-        color: #0d0d0d;
-        border: none;
-        font-family: 'Inter';
-        padding: 0px 12px;
-        height: 32px;
-        background: #ffffff;
-        top: 8px;
-        right: 8px;
-        box-shadow: 0 0 0 0.5px rgba(0, 0, 0, 0.08), 0 1px 2.5px rgba(0, 0, 0, 0.1);
-        cursor: pointer;
-    }
-
-    button#download:hover {
-        background: #f5f5f5;
-        box-shadow: 0 0 0 0.5px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.06), 0 6px 12px -3px rgba(0, 0, 0, 0.1);
-    }
-
-    button#download.downloading {
-        color: #ea580c;
-    }
-
-    button#download.done {
-        color: #16a34a;
-    }
-
-    ::-webkit-scrollbar {
-        width: 5px;
-        background-color: rgb(0 0 0 / 8%);
-    }
-
-    ::-webkit-scrollbar-thumb {
-        background-color: rgb(0 0 0 / 32%);
-        border-radius: 4px;
-    }
     `;
 
-    // HTML THAT IS RETURNED AS A RENDERABLE URL
+    // FINAL HTML
     const originalHTML = `
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.min.js"></script>
-      <style>${customCSS}</style>
-      <div class="main">
-      <div class="header">
-      <div id="content">${tableHTML}</div>
-      </div>
-      <button id="download">Download</button>
-      <script>
-      document.getElementById('download').addEventListener('click', function() {
-        var element = document.getElementById('content');
-        var button = this;
-        button.innerText = 'Downloading...';
-        button.className = 'downloading';
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.min.js"></script>
+        <style>${customCSS}</style>
+        <div class="main">
+            <div class="header">
+                <div id="content">${tableHTML}</div>
+            </div>
+            <button id="download">Download</button>
+            <script>
+                document.getElementById('download').addEventListener('click', function() {
+                    var element = document.getElementById('content');
+                    var button = this;
+                    button.innerText = 'Downloading...';
+                    button.className = 'downloading';
+                    var opt = {
+                        html2canvas: {
+                            useCORS: true,
+                        },
+                        jsPDF: {
+                            unit: 'px',
+                            hotfixes: ['px_scaling']
+                        }
+                    };
+                    html2pdf().set(opt).from(element).toPdf().get('pdf').then(function(pdf) {
+                        button.innerText = 'Done 🎉';
+                        button.className = 'done';
+                        setTimeout(function() { 
+                            button.innerText = 'Download';
+                            button.className = ''; 
+                        }, 2000);
+                    }).save();
+                });
+            </script>
+        </div>`;
 
-        var opt = {
-        html2canvas: {
-          useCORS: true,
-        },
-        jsPDF: {
-          unit: 'px',
-          hotfixes: ['px_scaling']
-        }
-        };
-        html2pdf().set(opt).from(element).toPdf().get('pdf').then(function(pdf) {
-        button.innerText = 'Done 🎉';
-        button.className = 'done';
-        setTimeout(function() { 
-          button.innerText = 'Download';
-          button.className = ''; 
-        }, 2000);
-        }).save();
-      });
-      </script>
-      `;
     var encodedHtml = encodeURIComponent(originalHTML);
     return "data:text/html;charset=utf-8," + encodedHtml;
 };
